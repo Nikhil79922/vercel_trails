76e37175ca58e4292032976fd669bcd753147bee
Nikhil79922 <ns94301918@gmail.com>
2025-02-12 11:22:42 +0530

    db check

--- index.js
+++ index.js
@@ -9,14 +9,12 @@ const DBconnection = require("./prisma/DBconnection");
 const app = express();
 const port = process.env.PORT || 8000;
 
-// Initialize DB connection
 DBconnection();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
     origin: "*",
-    methods: "GET POST PUT DELETE",
     credentials: true
 }));
 
